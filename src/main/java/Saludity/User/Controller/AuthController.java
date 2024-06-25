338dcf8be457a2813d05a9e18f8d5eca038d7a40
Boshos <ronst0004@gmail.com>
2024-06-24 22:04:48 -0500

    Seguridad

---
+++ src/main/java/Saludity/User/Controller/AuthController.java
@@ -0,0 +1,21 @@
+package Saludity.User.Controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login(){
+        return "Accesi exitoso";
+    }
+
+}
